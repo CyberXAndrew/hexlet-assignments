bac7b70082dda37149183ee0156314f1abd0d052
@hexlet/cli <support@hexlet.io>
2023-02-18 16:39:44 +0300

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,42 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+class DELETE { //"the java is the best programming language java" DELETE THIS CLASS DELETE THIS CLASS DELETE THIS CLASS
+    public static HashMap<String, Integer> getWordCount(String sentence) {
+        Map<String, Integer> dictionary = new HashMap<>();
+        ArrayList<String> array = Arrays.asList(sentence.split(" "));
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            index = 1;
+            String word = array.get(i);
+            for (int k = 0; k < array.size(); k++) {
+                if (array[i].equals(array[k] && i != k)) {
+                    index += 1;
+                    array.remove(array[k]);
+                }
+                numbers[i] = index;
+            }
+        }
+        int i = 0;
+        for (String key : dictionary.keySet()) {
+            key = array[i];
+            dictionary.put(key, numbers[i]);
+            i += 1;
+        }
+        return dictionary;
+    }
+
+    public static String toString(Map dictionary) {
+        String result = "{\n";
+        for (String key : dictionary.keySet()) {
+            result += key + ": " + dictionary.get(key) + "\n"; //   .put(key, numbers[i]);
+        }
+        result += "\n}";
+        return result;
+    }
+}
+//END  DELETE THIS CLASS DELETE THIS CLASS DELETE THIS CLASS DELETE THIS CLASS DELETE THIS CLASS DELETE THIS CLASS DELE
+
