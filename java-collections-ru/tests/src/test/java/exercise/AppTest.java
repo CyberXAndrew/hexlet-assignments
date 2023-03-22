61237e00b272fd92a8859015428d1b0b065993f9
@hexlet/cli <support@hexlet.io>
2023-03-22 20:43:21 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,4 +1,3 @@
-
 package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,16 +19,3 @@ class AppTest {
         // END
     }
 }
-
-//    List<Integer> num = new ArrayList <>(Arrays.asList(1, 2, 3, 4, 5));
-//        if (!take(num, 0).equals(new ArrayList <>(0))) {
-//        throw new AssertionError("error 0");
-//        } else if (!take(num, 1).equals(num.get(0))) {
-//        throw new AssertionError("error 1");
-//        } else if (!take(num, 3).equals(new ArrayList <>(Arrays.asList(1, 2, 3)))) {
-//        throw new AssertionError("error 3");
-//        } else if (!take(num, -1).equals(new ArrayList <>(0))) {
-//        throw new AssertionError("error -1");
-//        } else if (!take(num, 8).equals(num)) {
-//        throw new AssertionError("error 8");
-//        }
