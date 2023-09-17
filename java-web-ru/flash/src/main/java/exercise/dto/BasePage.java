ee16b853c0302d5d78cf844ad4e11416fe0e5b2f
@hexlet/cli <support@hexlet.io>
2023-09-17 19:39:57 +0300

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -0,0 +1,16 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// BEGIN
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasePage {
+    private String flash;
+}
+// END
